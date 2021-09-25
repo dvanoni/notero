15939de72ebf7b6bb8f6e7d35887b24f76e6c71b
David Vanoni <david@vanoni.dev>
2021-09-25 14:52:50 -0700

    Listen for items added to desired collection

--- content/notero.ts
+++ content/notero.ts
@@ -16,6 +16,28 @@ class Notero {
   private globals: Record<string, any>;
   private strings: any;
 
+  private notifierCallback = {
+    notify: (
+      event: string,
+      type: string,
+      ids: string[],
+      _: Record<string, unknown>
+    ) => {
+      if (event !== 'add' || type !== 'collection-item') return;
+
+      const collectionName = this.getPref('collectionName');
+
+      ids.forEach((collectionItem: string) => {
+        const [collectionID, itemID] = collectionItem.split('-');
+        const collection = Zotero.Collections.get(collectionID);
+
+        if (collection.name === collectionName) {
+          this.onItemAddedToCollection(itemID);
+        }
+      });
+    },
+  };
+
   // eslint-disable-next-line @typescript-eslint/require-await
   public async load(globals: Record<string, any>) {
     this.globals = globals;
@@ -24,6 +46,21 @@ class Notero {
     this.initialized = true;
 
     this.strings = globals.document.getElementById('notero-strings');
+
+    const notifierID = Zotero.Notifier.registerObserver(
+      this.notifierCallback,
+      ['collection-item'],
+      'notero'
+    );
+
+    // Unregister callback when the window closes
+    window.addEventListener(
+      'unload',
+      () => {
+        Zotero.Notifier.unregisterObserver(notifierID);
+      },
+      false
+    );
   }
 
   public openPreferences() {
@@ -35,6 +72,14 @@ class Notero {
 
   private getPref(pref): unknown {
     return Zotero.Prefs.get(`extensions.notero.${pref}`, true);
+  }
+
+  private onItemAddedToCollection(itemID: string) {
+    window.alert(`added ${itemID}`);
+
+    const item = Zotero.Items.get(itemID);
+
+    Zotero.debug(`Item URI: zotero://select/library/items/${item.key}`);
   }
 }
 

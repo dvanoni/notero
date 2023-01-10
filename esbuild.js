13d15dc637a76c2c78d50c306a47a00911e60f23
David Hoff-Vanoni <david@vanoni.dev>
2023-01-09 23:03:30 -0800

    build: Run builds in parallel

--- esbuild.js
+++ esbuild.js
@@ -4,15 +4,14 @@ require('@dvanoni/zotero-plugin/copy-assets');
 require('@dvanoni/zotero-plugin/generate-install-manifest');
 require('@dvanoni/zotero-plugin/version');
 
-async function build() {
-  await esbuild.build({
+const builds = [
+  {
     entryPoints: ['src/bootstrap.ts'],
     keepNames: true,
     outdir: 'build',
     target: ['firefox60'],
-  });
-
-  await esbuild.build({
+  },
+  {
     bundle: true,
     format: 'iife',
     target: ['firefox60'],
@@ -20,19 +19,18 @@ async function build() {
     outdir: 'build/content',
     banner: { js: 'if (!Zotero.Notero) {\n' },
     footer: { js: '\n}' },
-  });
-
-  await esbuild.build({
+  },
+  {
     bundle: true,
     format: 'iife',
     globalName: 'notero',
     target: ['firefox60'],
     entryPoints: ['src/content/preferences.ts'],
     outdir: 'build/content',
-  });
-}
+  },
+].map((buildOptions) => esbuild.build(buildOptions));
 
-build().catch((err) => {
-  console.log(err);
+Promise.all(builds).catch((err) => {
+  console.error(err);
   process.exit(1);
 });

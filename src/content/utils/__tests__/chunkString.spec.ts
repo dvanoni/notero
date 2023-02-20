5e1b20a8efa4847d1476437cdcb7f27fb61d6cf1
David Hoff-Vanoni <david@vanoni.dev>
2023-02-19 16:57:32 -0800

    test(chunkString): Use `it.each` to streamline tests

--- src/content/utils/__tests__/chunkString.spec.ts
+++ src/content/utils/__tests__/chunkString.spec.ts
@@ -1,19 +1,24 @@
 import { chunkString } from '../chunkString';
 
 describe('chunkString', () => {
-  it('returns empty array for empty string', () => {
-    expect(chunkString('', 5)).toStrictEqual([]);
-  });
-
-  it('returns single element when string is shorter than chunk size', () => {
-    expect(chunkString('1234', 5)).toStrictEqual(['1234']);
-  });
-
-  it('returns single element when string length is exactly chunk size', () => {
-    expect(chunkString('1234', 4)).toStrictEqual(['1234']);
-  });
-
-  it('returns multiple elements when string is longer than chunk size', () => {
-    expect(chunkString('1234', 3)).toStrictEqual(['123', '4']);
-  });
+  it.each`
+    str       | chunkSize | expected
+    ${''}     | ${5}      | ${[]}
+    ${'1234'} | ${5}      | ${['1234']}
+    ${'1234'} | ${4}      | ${['1234']}
+    ${'1234'} | ${3}      | ${['123', '4']}
+  `(
+    'returns $expected when string is "$str" and chunk size is $chunkSize',
+    ({
+      str,
+      chunkSize,
+      expected,
+    }: {
+      str: string;
+      chunkSize: number;
+      expected: string[];
+    }) => {
+      expect(chunkString(str, chunkSize)).toStrictEqual(expected);
+    }
+  );
 });

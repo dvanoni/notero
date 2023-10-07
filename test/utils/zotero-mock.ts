5d30893fb80dec8c77a11b76b20af5f5883f2dd2
David Hoff-Vanoni <david@vanoni.dev>
2023-10-06 23:56:13 -0700

    test: Provide a way to mock Zotero prefs

--- test/utils/zotero-mock.ts
+++ test/utils/zotero-mock.ts
@@ -1,3 +1,10 @@
 import { DeepMockProxy } from 'jest-mock-extended';
 
 export const zoteroMock = Zotero as DeepMockProxy<typeof Zotero>;
+
+export function mockZoteroPrefs() {
+  const prefsStore = new Map<string, Zotero.Prefs.Value>();
+
+  zoteroMock.Prefs.get.mockImplementation(prefsStore.get.bind(prefsStore));
+  zoteroMock.Prefs.set.mockImplementation(prefsStore.set.bind(prefsStore));
+}

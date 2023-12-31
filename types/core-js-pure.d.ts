1670c13cd1190d35ba06d88c929f1096a67ffcf7
David Hoff-Vanoni <david@vanoni.dev>
2023-12-30 21:28:43 -0800

    refactor: Replace `core-js` with `core-js-pure` to avoid pollution
    Closes #319

---
+++ types/core-js-pure.d.ts
@@ -0,0 +1,9 @@
+declare module 'core-js-pure/es/string/trim-end' {
+  function trimEnd(str: string): string;
+  export = trimEnd;
+}
+
+declare module 'core-js-pure/es/string/trim-start' {
+  function trimStart(str: string): string;
+  export = trimStart;
+}

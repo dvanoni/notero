87f76a2fecb07c386bfd15c339b154934e88d8d9
David Hoff-Vanoni <david@vanoni.dev>
2024-05-03 23:41:41 -0700

    fix: Support changes to ProgressWindow API (#502)
    * feat: Remove Notion icon from progress window
    
    * fix: Support changes to ProgressWindow API

--- src/content/sync/progress-window.ts
+++ src/content/sync/progress-window.ts
@@ -1,23 +1,23 @@
-const NOTION_LOGO = 'chrome://notero/content/style/notion-logo-32.png';
-
 const getTickIcon = () => `chrome://zotero/skin/tick${Zotero.hiDPISuffix}.png`;
 
 export class ProgressWindow {
   private readonly itemCount: number;
   private readonly itemProgress: Zotero.ProgressWindow.ItemProgress;
   private readonly progressWindow: Zotero.ProgressWindow;
+  private readonly useNewIcons: boolean;
 
   public constructor(itemCount: number) {
     this.itemCount = itemCount;
 
     this.progressWindow = new Zotero.ProgressWindow();
-    this.progressWindow.changeHeadline(
-      'Syncing items to Notion...',
-      NOTION_LOGO,
-    );
+    this.progressWindow.changeHeadline('Syncing items to Notion…');
     this.progressWindow.show();
 
-    this.itemProgress = new this.progressWindow.ItemProgress('', '');
+    this.itemProgress = new this.progressWindow.ItemProgress('document', '');
+
+    // See https://github.com/zotero/zotero/pull/4047
+    this.useNewIcons =
+      typeof this.itemProgress.setItemTypeAndIcon === 'function';
   }
 
   public updateText(step: number) {
@@ -30,14 +30,19 @@ export class ProgressWindow {
   }
 
   public complete() {
-    this.itemProgress.setIcon(getTickIcon());
+    if (typeof this.itemProgress.setIcon === 'function') {
+      this.itemProgress.setIcon(getTickIcon());
+    }
     this.progressWindow.startCloseTimer();
   }
 
   public fail(errorMessage: string, failedItem?: Zotero.Item) {
     if (failedItem) {
+      const iconSrc = this.useNewIcons
+        ? failedItem.itemType
+        : Zotero.ItemTypes.getImageSrc(failedItem.itemType);
       new this.progressWindow.ItemProgress(
-        Zotero.ItemTypes.getImageSrc(failedItem.itemType),
+        iconSrc,
         failedItem.getDisplayTitle(),
         this.itemProgress,
       ).setProgress(100);

74216a58ca25af07aec6e4a28b2af3ed128dde57
David Hoff-Vanoni <david@vanoni.dev>
2024-09-06 23:33:13 -0700

    refactor: Remove obsolete `ChromeManager`

--- src/content/services/index.ts
+++ src/content/services/index.ts
@@ -1,6 +1,5 @@
 export type { Service } from './service';
 
-export { ChromeManager } from './chrome-manager';
 export { EventManager } from './event-manager';
 export { PreferencePaneManager } from './preference-pane-manager';
 export { SyncManager } from './sync-manager';

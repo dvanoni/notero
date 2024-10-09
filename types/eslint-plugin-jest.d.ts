ae38c81c27bdbcf0a5b7e74ee358b8286383a701
David Hoff-Vanoni <david@vanoni.dev>
2024-10-08 23:55:09 -0700

    build(deps-dev): Remove jest and related packages

--- types/eslint-plugin-jest.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'eslint-plugin-jest' {
-  import type { TSESLint } from '@typescript-eslint/utils';
-
-  const configs: {
-    'flat/all': TSESLint.FlatConfig.Config;
-    'flat/recommended': TSESLint.FlatConfig.Config;
-    'flat/style': TSESLint.FlatConfig.Config;
-  };
-}
